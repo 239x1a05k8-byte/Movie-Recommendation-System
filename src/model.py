588f88e328b65664dd8ec70a7b0a8152bd70cfa9
Sakkari Jhansi Rani <sakkarijhansirani@gmail.com>
2026-06-10 13:51:23 +0530

    Fix file path issue for Streamlit deployment

--- src/model.py
+++ src/model.py
@@ -1,24 +1,30 @@
 import pickle
+import os
+import numpy as np
 
-movies = pickle.load(open('model/movies.pkl', 'rb'))
-similarity = pickle.load(open('model/similarity.pkl', 'rb'))
+# Get current directory of this file
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+# Go one level up to project root
+PROJECT_ROOT = os.path.dirname(BASE_DIR)
+
+# Correct paths
+movies_path = os.path.join(PROJECT_ROOT, "model", "movies.pkl")
+similarity_path = os.path.join(PROJECT_ROOT, "model", "similarity.pkl")
+
+# Load files safely
+movies = pickle.load(open(movies_path, "rb"))
+similarity = pickle.load(open(similarity_path, "rb"))
 
 def recommend(movie):
     movie_index = movies[movies['title'] == movie].index[0]
 
     distances = similarity[movie_index]
 
-    movies_list = sorted(
-        list(enumerate(distances)),
-        reverse=True,
-        key=lambda x: x[1]
-    )[1:6]
+    movie_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
 
-    recommended_movies = []
+    recommendations = []
+    for i in movie_list:
+        recommendations.append(movies.iloc[i[0]].title)
 
-    for i in movies_list:
-        recommended_movies.append(
-            movies.iloc[i[0]].title
-        )
-
-    return recommended_movies
+    return recommendations
